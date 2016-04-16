531ee3710ecee61a4744393fb0d823169bac05ff
kattiiz00789 <katz_up2u@hotmail.com>
2016-04-16 21:36:14 +0700

    ekatto

---
+++ loopfor.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class loopfor {
+	public static void main(String[] args) {
+        //Scanner Sc = new Scanner(System.in);  
+        for(int i=0;i<20;i++){
+        	for(int j=0;j<i;j++){
+        		System.out.print("*");
+
+        	}
+        	System.out.println("*");
+        }    
+      
+    
+
+
+    }
+
+}
