411605d25305514872b02384282ad4992497ed29
kattiiz00789 <katz_up2u@hotmail.com>
2016-04-20 23:47:19 +0700

    ekatto

---
+++ condition3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class condition3{
+ 	public static void main(String[] args){
+ 		Scanner g = new Scanner(System.in);
+ 		int x=1;
+ 		switch(x){
+ 			case 1:
+ 				System.out.print(x);
+ 				break;
+ 			case 2:
+ 				System.out.print(x);
+ 				break;
+ 			case 3:
+ 				System.out.print(x);
+ 				break;
+ 			case 0:
+ 				System.out.print(x);		
+                break;
+ 		   }
+ 	 	}
+}
