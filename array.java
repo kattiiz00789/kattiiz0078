7f9b88c721e1994b2019006d6c81e09df7be501a
kattiiz00789 <katz_up2u@hotmail.com>
2016-05-03 20:01:47 +0700

    Update array.java

--- array.java
+++ array.java
@@ -7,12 +7,15 @@ class  array{
 		System.out.print("Input leght : ");
 		y = sc.nextInt();
 		int[] a = new int[y];
-		for(int i=1;i<=a.length;i++){			
+                for(int i=1;i<=a.length;i++){
 			System.out.print("Input number : "+i+" = ");
-	        d = sc.nextInt();
-	        if(d%2 == 0){	
-	        	a[i] = d;   
-	        	System.out.println("ANS. = "+a[i]);
+			int d = sc.nextInt();
+			a[i] = d;
+		}	
+		for(int t=0;t<=a.length;t++){	
+	        if(a[t]%2 == 0){	
+	       	 System.out.printf(" "+a[t]+" ");			   	        	
+	        	   	        	   	 	        
 
 	      	}				
 		}                
